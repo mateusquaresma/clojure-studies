2e2d744044f4229cbfda458d3b4a71b8e9682a66
mateusquaresma <mateus.quaresma@protonmail.com>
2020-05-14 09:16:02 -0300

    Implemented chapter 4 - exercise 3

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -35,11 +35,46 @@
   [minimum-glitter records]
   (filter #(>= (:glitter-index %) minimum-glitter) records))
 
+(defn glitter-filter-list
+  [minimum-glitter records]
+  (map #(:name %) (filter #(>= (:glitter-index %) minimum-glitter) records)))
+
 ;; Exercises
 
+;; Exercise 1
 (defn listfy
   [map-records]
   (reduce (fn [acc row]
             (conj acc (:name row)))
           []
           map-records))
+
+;; Exercise 2
+(defn append
+  "Appends a new suspect to a list of suspects"
+  [suspects new-suspect]
+  (conj suspects new-suspect))
+
+;; Exercise 3
+
+(defn valid-name?
+  [name]
+  (and (not (nil? name)) (> (count name) 0)))
+
+(defn valid-glitter-index?
+  [glitter-index]
+  (not (nil? glitter-index)))
+
+(def validations {:name valid-name? :glitter-index valid-glitter-index?})
+
+(defn validate
+  [validations record]
+  (and ((:name validations) (:name record))
+       ((:glitter-index validations) (:glitter-index record))))
+
+(defn append
+  "Appends a new suspect to a list of suspects"
+  [validations suspects new-suspect]
+  (if (validate validations new-suspect)
+    (conj suspects new-suspect)
+    suspects))
