b557d648e92c3b154575acb03404b207b4c02bc3
mateusquaresma <mateus.quaresma@protonmail.com>
2020-05-18 08:55:37 -0300

    Peg Thing's board is done

--- cbt-chapters/src/cbt_chapters/chapter_5.clj
+++ cbt-chapters/src/cbt_chapters/chapter_5.clj
@@ -1,1 +1,161 @@
 (ns cbt-chapters.chapter-5)
+
+;; unrelated
+
+(defn e
+  [n]
+  (Math/pow (+ 1.0 (/ 1.0  n)) n))
+
+(def character
+  {:name "Smooches McCutes"
+   :attributes {:intelligence 10
+                :strength 4
+                :dexterity 5}})
+
+(def c-int (comp :intelligence :attributes))
+(def c-str (comp :strength :attributes))
+(def c-dex (comp :dexterity :attributes))
+
+(c-int character)
+(c-str character)
+(c-dex character)
+
+(defn spell-slots
+  [char]
+  (int (inc (/ (c-int char) 2))))
+
+(def spell-slots-comp (comp int inc #(/ % 2) c-int))
+
+(defn two-comp
+  [f g]
+  (fn [& args]
+    (f (apply g args))))
+
+(def c-dex-2 (two-comp :dexterity :attributes))
+
+;; memoization
+
+(defn sleepy-identity
+  "Returns the given value after 1 second"
+  [x]
+  (Thread/sleep 1000)
+  x)
+
+(sleepy-identity "Mr Fantastico")
+(def memo-sleepy-identity (memoize sleepy-identity))
+
+;; Peg Thing
+
+{1  {:pegged true, :connections {6 3, 4 2}},
+ 2  {:pegged true, :connections {9 5, 7 4}},
+ 3  {:pegged true, :connections {10 6, 8 5}},
+ 4  {:pegged true, :connections {13 8, 11 7, 6 5, 1 2}},
+ 5  {:pegged true, :connections {14 9, 12 8}},
+ 6  {:pegged true, :connections {15 10, 13 9, 4 5, 1 3}},
+ 7  {:pegged true, :connections {9 8, 2 4}},
+ 8  {:pegged true, :connections {10 9, 3 5}},
+ 9  {:pegged true, :connections {7 8, 2 5}},
+ 10 {:pegged true, :connections {8 9, 3 6}},
+ 11 {:pegged true, :connections {13 12, 4 7}},
+ 12 {:pegged true, :connections {14 13, 5 8}},
+ 13 {:pegged true, :connections {15 14, 11 12, 6 9, 4 8}},
+ 14 {:pegged true, :connections {12 13, 5 9}},
+ 15 {:pegged true, :connections {13 14, 6 10}},
+ :rows 5}
+
+; Notes
+; keys and values have different types
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+;; To test the function above
+(def tri (tri*))
+(take 5 tri)
+
+(defn triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15 etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns the row number the position belongs to:
+  position 1 in row 1, positions 2 and 3 in row 2, etc"
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos pos neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board
+            [[pos destination] [destination pos]])
+    board))
+
+(connect {} 15 1 2 4)
+
+;; Examples of assoc-in
+
+(assoc-in {} [:cookie :monster :vocals] "Finntroll")
+(get-in {:cookie {:monster {:vocals "Finntroll"}}}
+        [:cookie :monster])
+(assoc-in {} [1 :connections 4] 2)
+
+(defn connect-right
+  [board max-pos pos]
+  (let [neighbor (inc pos)
+        destination (inc neighbor)]
+    (if-not (or (triangular? neighbor) (triangular? pos))
+      (connect board max-pos pos neighbor destination)
+      board)))
+
+(defn connect-down-left
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ row pos)
+        destination (+ 1 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn connect-down-right
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ 1 row pos)
+        destination (+ 2 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+;; Examples
+(connect-down-left {} 15 1)
+(connect-down-right {} 15 3)
+
+(defn add-pos
+  "Pegs the position and performs connection"
+  [board max-pos pos]
+  (let [pegged-board (assoc-in board [pos :pegged] true)]
+    (reduce (fn [new-board connection-creation-function]
+              (connection-creation-function new-board max-pos pos))
+            pegged-board
+            [connect-right connect-down-left connect-down-right]
+            )))
+(defn new-board
+  "Creates a new board with the given number of rows"
+  [rows]
+  (let [initial-board {:rows rows}
+        max-pos (row-tri rows)]
+    (reduce (fn [board pos]
+              (add-pos board max-pos pos))
+            initial-board
+            (range 1 (inc max-pos)))))
+
+;; Moving Pegs
+
