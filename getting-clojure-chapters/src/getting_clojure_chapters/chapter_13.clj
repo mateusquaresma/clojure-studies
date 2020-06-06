d989708a5753ab3b8b66657a4e9cdf14605d3f32
Mateus Quaresma <mateus.quaresma@ifood.com.br>
2020-06-06 19:12:45 -0300

    Finished chapter 13

--- getting-clojure-chapters/src/getting_clojure_chapters/chapter_13.clj
+++ getting-clojure-chapters/src/getting_clojure_chapters/chapter_13.clj
@@ -9,3 +9,43 @@
 
 (:name elisabeth)
 (:appears-in watson)
+
+(defrecord Employee [first-name last-name department])
+(def alice (->Employee "Alice" "Smith" "Engineering"))
+
+(defprotocol Person
+  (full-name [this])
+  (greeting [this msg])
+  (description [this]))
+
+(defrecord FictionalCharacter [name appears-in author]
+  Person
+  (full-name [this] (:name this))
+  (greeting [this msg]
+    (str msg " " (:name this)))
+  (description [this]
+    (str (:name this) " is a character and appears in " (:appears-in this))))
+
+(defrecord Employee [first-name last-name department]
+  Person
+  (full-name [this] (str first-name " " last-name))
+  (greeting [this msg] (str " " first-name))
+  (description [this] (str (:first-name this) " works in " (:department this))))
+
+(def sofia (->Employee "Sofia" "Diego" "Finance"))
+(def sam (->FictionalCharacter "Sam Weller" "The Pickwick Papers" "Dickens"))
+
+(full-name sofia)
+(description sam)
+(greeting sofia "Hello")
+
+(defprotocol Marketable
+  (make-slogan [this]))
+
+(extend-protocol Marketable
+  Employee
+  (make-slogan [e] (str (:first-name e) " is the best employee!")))
+
+(extend-protocol Marketable
+  FictionalCharacter
+  (make-slogan [fc] (str (:name fc) " is the greatest character!")))
