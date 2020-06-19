2dda6ef8e1f8957cc4fe43701f9fb814f54ba84f
Mateus Quaresma <mateus.quaresma@protonmail.com>
2020-06-19 09:42:31 -0300

    A little progress on chapter 9, concurrent programming

--- cbt-chapters/src/cbt_chapters/chapter_9.clj
+++ cbt-chapters/src/cbt_chapters/chapter_9.clj
@@ -38,3 +38,46 @@
 (def my-promise (promise))
 (deliver my-promise (+ 1 2))
 @my-promise
+
+(def yak-butter-international
+  {:store "Yak Butter International"
+   :price 90
+   :smoothness 90})
+
+(def butter-than-nothing
+  {:store "Butter Than Nothing"
+   :price 150
+   :smoothness 83})
+
+(def baby-got-yak
+  {:store "Baby Got Yak"
+   :price 94
+   :smoothness 99})
+
+(defn mock-api-call [result]
+  (Thread/sleep 1000)
+  result)
+
+(defn satisfactory?
+  "If the butter meets our criteria, return the butter, else return false"
+  [butter]
+  (and
+    (<= (:price butter) 100)
+    (>= (:smoothness butter) 97)
+    butter))
+
+(time (some
+  (comp satisfactory? mock-api-call)
+  [yak-butter-international butter-than-nothing baby-got-yak]))
+
+(time
+  (let [butter-promise (promise)]
+    (doseq [butter [yak-butter-international butter-than-nothing baby-got-yak]]
+      (future (if-let [satisfactory-butter (satisfactory? (mock-api-call butter))]
+                (deliver butter-promise satisfactory-butter))))
+    (println "The winner is: "  (deref butter-promise 1500 "Time out"))))
+
+(let [ferengi-wisdom-promise (promise)]
+  (future (println "Here is some Ferengi wisdom: " @ferengi-wisdom-promise))
+  (Thread/sleep 100)
+  (deliver ferengi-wisdom-promise "Whisper your way to success!"))
